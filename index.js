2829385e67b5a5caf77ba9518dffd562b336afa7
sujon <jasujon570@gmail.com>
2025-09-08 20:15:44 +0600

    Highlight active category button when selected Completed

--- index.js
+++ index.js
@@ -1,71 +1,78 @@
-const loadCategories=()=> {
-    fetch('https://openapi.programming-hero.com/api/categories')
+const loadCategories = () => {
+  fetch("https://openapi.programming-hero.com/api/categories")
     .then((res) => res.json())
-    .then(json => displayCategories(json.categories))
-}
-const loadPlants = (id)=> {
-    const url = `https://openapi.programming-hero.com/api/category/${id}`
-    fetch(url)
+    .then((json) => displayCategories(json.categories));
+};
+const loadPlants = (id) => {
+  const url = `https://openapi.programming-hero.com/api/category/${id}`;
+  fetch(url)
     .then((res) => res.json())
-    .then(data => displayTree(data.plants))
-}
+    .then((data) => {
+      removeActive();
+      const clickCat = document.getElementById(`cat-${id}`);
+      clickCat.classList.add("active");
+      // console.log(clickCat);
+      displayTree(data.plants);
+    });
+};
+
+const removeActive = () => {
+  const categoryButtons = document.querySelectorAll(".cat-btn");
+  console.log(categoryButtons);
+  categoryButtons.forEach((btn) => btn.classList.remove('active'));
+};
 
 const displayTree = (plants) => {
-    const treeContainer = document.getElementById('tree-container');
-    treeContainer.innerHTML = '';
+  const treeContainer = document.getElementById("tree-container");
+  treeContainer.innerHTML = "";
 
-    plants.forEach((plant) => {
-        console.log(plant);
-        const cardDiv = document.createElement("div");
-        cardDiv.classList.add("bg-white", "w-[343px]", "p-4", "rounded-lg");
+  plants.forEach((plant) => {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add(
+      "bg-white",
+      "w-[343px]",
+      "p-4",
+      "rounded-lg",
+      "h-[500px]",
+      "flex",
+      "flex-col"
+    );
 
-        cardDiv.innerHTML = `
-            <img class="w-full h-[187px] rounded-lg" src="${plant.image}" alt="${plant.name}">
-            <div>
-                <h3 class="font-semibold text-xl">${plant.name}</h3>
-                <p class="text-[#1F2937]">${plant.description}</p>
-                <div class="flex justify-between">
+    cardDiv.innerHTML = `
+        <img class="w-full h-[200px] rounded-lg mb-3" src="${plant.image}" alt="${plant.name}">
+        <div class="flex flex-col flex-grow">
+            <h3 class="font-semibold text-xl">${plant.name}</h3>
+            <p class="text-[#1F2937]">${plant.description}</p>
+            <div class="mt-auto">
+                <div class="flex justify-between mb-3">
                     <button class="text-[#15803D] bg-[#DCFCE7] py-1 px-3 rounded-[400px]">${plant.category}</button>
                     <p><span>৳</span>${plant.price}</p>
                 </div>
-                <button class="w-full bg-[#15803D] text-white font-medium py-3 rounded-[999px]">Add to Cart</button>
+                <button class="w-full bg-[#15803D] text-white font-medium py-3 rounded-[999px] mt-2">Add to Cart</button>
             </div>
+        </div>
             
         `;
 
-        treeContainer.append(cardDiv);
-
-    })
-}
-
+    treeContainer.append(cardDiv);
+  });
+};
 
 const displayCategories = (categories) => {
-    const categoriesContainer = document.getElementById('categories-container')
-    categoriesContainer.innerHTML = '';
+  const categoriesContainer = document.getElementById("categories-container");
+  categoriesContainer.innerHTML = "";
 
-    for (const category of categories) {
-        console.log(category);
-        const categoryList = document.createElement("div");
-        categoryList.innerHTML = `
-            <button onclick="loadPlants(${category.id})" class="category-btn w-full text-left p-2 rounded hover:bg-green-100 focus:bg-green-600 focus:text-white"
+  for (const category of categories) {
+    const categoryList = document.createElement("div");
+    categoryList.innerHTML = `
+            <button id="cat-${category.id}" onclick="loadPlants(${category.id})" class="category-btn w-full text-left p-2 rounded cat-btn"
             >
                 ${category.category_name}
             </button>
         `;
 
-
-        categoriesContainer.append(categoryList);
-    }
-}
-
-
-
-
-
-
-
-
-
-
+    categoriesContainer.append(categoryList);
+  }
+};
 
 loadCategories();
